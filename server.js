3e7071b6800ab688bab671d88a35e5fc6e81b49b
Raylson Carlos <raylsoncs@gmail.com>
2020-11-18 19:58:26 -0300

    minor changes in database connection

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ require("./app/routes/customer.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log("Server is running on port ${PORT}.");
+  console.log(`Server is running on port ${PORT}.`);
 });
