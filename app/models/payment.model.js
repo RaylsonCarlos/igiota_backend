fc15c79e93d7a2fe2d030f4f840284a8a2f9eb52
Raylson Carlos <raylsoncs@gmail.com>
2020-12-21 20:22:36 -0300

    consulta de pagamentos agora vai com info de usuário

--- app/models/payment.model.js
+++ app/models/payment.model.js
@@ -32,7 +32,13 @@ Payment.create = (newPayment, userId, token, result) => {
 };
 
 Payment.findAll = (debtId , result) => {
-    sql.query(`SELECT * FROM Payment WHERE debt_id = ${debtId}`, (err, res) => {
+    sql.query(`SELECT *, OWNER.id AS DEBT_OWNER_ID, OWNER.name AS DEBT_OWNER_NAME, OWNER.email AS DEBT_OWNER_EMAIL,
+                    OWE_TO.id AS DEBT_OWE_TO_ID, OWE_TO.name AS DEBT_OWE_TO_NAME, OWE_TO.email AS DEBT_OWE_TO_EMAIL                    
+                    FROM Payment 
+                        INNER JOIN Debt ON Payment.debt_id = Debt.Id
+                        INNER JOIN User AS OWNER ON Debt.Owner = OWNER.id
+                        INNER JOIN User AS OWE_TO ON Debt.Owe_to = OWE_TO.id
+                            WHERE debt_id = ${debtId}`, (err, res) => {
         if(err){
             result(err, null);
             return;
